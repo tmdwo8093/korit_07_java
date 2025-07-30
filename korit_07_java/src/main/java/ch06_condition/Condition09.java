a8fe8218b4c76f15b8ec3e0673d5a7807f699094
tmdwo8093 <tmdwo8093@gmail.com>
2025-07-30 16:51:41 +0900

    first commit

--- korit_07_java/src/main/java/ch06_condition/Condition09.java
+++ korit_07_java/src/main/java/ch06_condition/Condition09.java
@@ -2,20 +2,18 @@ package ch06_condition;
 
 import java.util.Scanner;
 
-/*
-
-        논리 연산자를 이용한 풀이
- */
 public class Condition09 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("연도를 입력하시오 >>> ");
+        System.out.print("연도를 입력하세요 >>> ");
         int year = scanner.nextInt();
-        String leapYear = "윤년입니다";
+        String leapYear = "윤년입니다.";
 
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-        } else {
+        if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {}
+        else {
             leapYear = "윤년이 아닙니다.";
         }
+
+        System.out.println(year + "년은 " + leapYear);
     }
 }
